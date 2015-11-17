9069038693abab81c8aeab73d69fbd02c24635ed
J ª/\/\ ∑ $ <james@galvanize.com>
2015-11-16 21:51:12 -0700

    Create rock-paper-scissors.js

---
+++ js/unit-2/rock-paper-scissors.js
@@ -0,0 +1,9 @@
+// Rock paper scissors is a classic 2 player game. Each player chooses either rock, paper or scissors. The possible outcomes: Rock destroys scissors. Scissors cut paper. Paper covers rock.
+
+var userChoice = function(answer){
+    var answer = prompt("Do you choose rock, paper or scissors?")
+}
+
+var computerChoice = Math.random();
+
+console.log(computerChoice);
