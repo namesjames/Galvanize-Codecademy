b21ea3d4c69da6bbf39c55d5ebae60a00badccf3
J ª/\/\ ∑ $ <james@galvanize.com>
2015-11-17 23:25:10 -0700

    Update rock-paper-scissors.js

--- js/unit-2/rock-paper-scissors.js
+++ js/unit-2/rock-paper-scissors.js
@@ -6,4 +6,13 @@ var userChoice = function(answer){
 
 var computerChoice = Math.random();
 
+if (computerChoice < 0.34) {
+computerChoice = "rock";
+} 
+else if(computerChoice <= 0.67) {
+computerChoice = "paper";
+} 
+else {
+computerChoice = "scissors";
+} 
 console.log(computerChoice);
