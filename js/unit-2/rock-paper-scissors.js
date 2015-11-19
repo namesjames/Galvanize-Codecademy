f813f8e5d413db2f4e27493003b2efc7d6523957
J ª/\/\ ∑ $ <james@galvanize.com>
2015-11-18 17:22:40 -0700

    Update rock-paper-scissors.js

--- js/unit-2/rock-paper-scissors.js
+++ js/unit-2/rock-paper-scissors.js
@@ -16,3 +16,9 @@ else {
 computerChoice = "scissors";
 } 
 console.log(computerChoice);
+
+var compare = function(choice1, choice2) {
+    if (choice1 === choice2) {
+        return("The result is a tie!");
+    }
+}
