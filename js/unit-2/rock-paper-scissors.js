c3a274c96367bced9b8fbd750f7eb3d32428e701
J ª/\/\ ∑ $ <james@galvanize.com>
2015-11-19 21:22:07 -0700

    Update rock-paper-scissors.js

--- js/unit-2/rock-paper-scissors.js
+++ js/unit-2/rock-paper-scissors.js
@@ -18,7 +18,41 @@ computerChoice = "scissors";
 console.log(computerChoice);
 
 var compare = function(choice1, choice2) {
-    if (choice1 === choice2) {
-        return("The result is a tie!");
+    if(choice1 === choice2) {
+        return "The result is a tie";
     }
+    else if(choice1 === "rock") {
+    }
+        if (choice2 === "scissors") {
+            return "rock wins";
+        }
+        else { 
+            return "paper wins";
+        }
+}
+var compare = function(choice1, choice2) {
+    if(choice1 === choice2) {
+        return "The result is a tie";
+    }
+    else if(choice1 === "paper") {
+    }
+        if (choice2 === "rock") {
+            return "paper wins";
+        }
+        else { 
+            return "scissors wins";
+        }
+}
+var compare = function(choice1, choice2) {
+    if(choice1 === choice2) {
+        return "The result is a tie";
+    }
+    else if(choice1 === "paper") {
+    }
+        if (choice2 === "rock") {
+            return "paper wins";
+        }
+        else { 
+            return "scissors wins";
+        }
 }
