e1976366a2b996b7f9cca389deb42989350bbf04
J ª/\/\ ∑ $ <james@galvanize.com>
2015-11-23 23:30:11 -0700

    Update script.js

--- js/unit-4/script.js
+++ js/unit-4/script.js
@@ -1,3 +1,4 @@
+// 4.1
 var coinFace = Math.floor(Math.random() * 2);
 
 while(coinFace === 0){
@@ -6,5 +7,35 @@ while(coinFace === 0){
 }
 console.log("Tails! Done flipping.");
 
-// 4.1 
+// 4.2 
+
+var understand = true;
+
+while(understand){
+	console.log("I'm learning while loops!");
+	understand = false;
+}
+
+// 4.3
+
+understand = true;
+
+while(understand){
+	console.log("I'm learning while loops!");
+	//Change the value of 'understand' here!
+	understand = false;
+}
+
+// 4.4
+
+var bool = true;
+
+while(bool){
+    console.log("Less is more!");
+    bool = false;
+}
+
+// 4.5
+
+
 
