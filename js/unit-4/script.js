a6c27a5a4c0597063f7e2548319986b5d6492f2a
J ª/\/\ ∑ $ <james@galvanize.com>
2015-11-29 02:52:23 -0700

    Update script.js

--- js/unit-4/script.js
+++ js/unit-4/script.js
@@ -36,6 +36,25 @@ while(bool){
 }
 
 // 4.5
+//Write a while loop that logs "I'm looping!" to the console three times. You can do this however you like, but NOT with three console.log calls.
+    var loops = 0; 
+    while(loops<3) { 
+    console.log("I'm looping!");
+    loops++; }
+    
+// 4.6
+// Inside the soloLoop function, write a while loop that takes an initial condition that's true. Your loop should log "Looped once!" to the console, then change that initial condition to false.
+var soloLoop = function(){
+  while(soloLoop) {
+  console.log("Looped once!");
+  soloLoop = false;
+}
+};
+
+soloLoop(true);
+    
+// 4.7 
+
 
 
 
