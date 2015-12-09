24b7d559f2fbc33fa62bb66267a841ed1b222977
J ª/\/\ ∑ $ <james@galvanize.com>
2015-12-08 23:44:44 -0700

    Create addressBook.js

---
+++ js/unit-7/addressBook.js
@@ -0,0 +1,30 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-777",
+    email: "bob.jones@example.com"
+};
+
+console.log(bob.firstName);
+console.log(bob.lastName);
+console.log(bob.email);
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888 - 8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+console.log(mary.phoneNumber);
+
+var printPerson = function(person) {
+    console.log(person.firstName + " " + person.lastName);    
+};
+
+printPerson(contacts[0]);
+
+printPerson(contacts[1]);
+
