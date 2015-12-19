c73b9a06244176ad9dffd4d996640005cc256221
J ª/\/\ ∑ $ <james@galvanize.com>
2015-12-19 09:06:08 -0700

    Update objects.js

--- js/unit-7/objects.js
+++ js/unit-7/objects.js
@@ -299,5 +299,116 @@ family[2] = new Person("michelle", 8);
 // add the last family member, "timmy", who is 6 years old
 family[3] = new Person("timmy" , 6);
 
+// Our Person constructor
+function Person (name, age) {
+    this.name = name;
+    this.age = age;
+}
 
+// Now we can make an array of people
+var family = new Array();
+
+family[0] = new Person("alice", 40);
+family[1] = new Person("bob", 42);
+family[2] = new Person("michelle", 8);
+family[3] = new Person("timmy" , 6);
+
+// loop through our new array
+
+for (var i = 0; i < family.length; i++) {
+    console.log(family[i].name);
+}
+
+// Our person constructor
+function Person (name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// We can make a function which takes persons as arguments
+// This one computes the difference in ages between two people
+var ageDifference = function(person1, person2) {
+    return person1.age - person2.age;
+}
+
+var alice = new Person("Alice", 30);
+var billy = new Person("Billy", 25);
+
+// get the difference in age between alice and billy using our function
+var diff = ageDifference(alice, billy)
+
+// Our person constructor
+function Person (name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// We can make a function which takes persons as arguments
+// This one computes the difference in ages between two people
+var ageDifference = function(person1, person2) {
+    return person1.age - person2.age;
+};
+
+// Make a new function, olderAge, to return the age of
+// the older of two people
+var olderAge = function (alice, billy) {
+    if (alice.age > billy.age){
+    return (alice.age)
+    }
+    else {
+        return (billy.age)
+    }
+};
+// Let's bring back alice and billy to test our new function
+var alice = new Person("Alice", 30);
+var billy = new Person("Billy", 25);
+
+console.log("The older person is " + olderAge(alice, billy));
+
+var spencer = {
+  age: 22,
+  country: "United States"
+};
+
+// make spencer2 here with constructor notation
+var spencer2 = new Object();
+    spencer2.age = 22;
+    spencer2.country = "United States";
+    
+    var snoopy = new Object();
+snoopy.species = "beagle";
+snoopy.age = 10;
+
+// save Snoopy's age and species into variables
+// use dot notation for snoopy's species
+var species = snoopy.species
+    
+// use bracket notation for snoopy's age
+var age = snoopy["age"]
+
+// 3 lines required to make harry_potter
+var harry_potter = new Object();
+harry_potter.pages = 350;
+harry_potter.author = "J.K. Rowling";
+
+// A custom constructor for book
+function Book (pages, author) {
+    this.pages = pages;
+    this.author = author;
+}
+
+// Use our new constructor to make the_hobbit in one line
+var the_hobbit = new Book(320, "J.R.R. Tolkien")
+
+function Circle (radius) {
+    this.radius = radius;
+    this.area = function () {
+        return Math.PI * this.radius * this.radius;
+        
+    };
+    // define a perimeter method here
+    this.perimeter = function() {
+        return 2 * Math.PI * this.radius;
+    }
+};
 
