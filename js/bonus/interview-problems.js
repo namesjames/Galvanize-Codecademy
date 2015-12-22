6c6489944533823e57978d05630a1eb2488a6876
J ª/\/\ ∑ $ <james@galvanize.com>
2015-12-22 16:13:10 -0700

    Update interview-problems.js

--- js/bonus/interview-problems.js
+++ js/bonus/interview-problems.js
@@ -81,3 +81,22 @@ function greatestCommonDivisor(a, b){
 > greatestCommonDivisor(69, 169);
   = 1
   
+// Remove Duplicates
+
+function removeDuplicate(arr){
+  var exists ={},
+      outArr = [], 
+      elm;
+
+  for(var i =0; i<arr.length; i++){
+    elm = arr[i];
+    if(!exists[elm]){
+      outArr.push(elm);
+      exists[elm] = true;
+   }
+  }
+  return outArr;
+}
+
+> removeDuplicate([1,3,3,3,1,5,6,7,8,1]);
+  = [1, 3, 5, 6, 7, 8]
