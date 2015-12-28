5fc0ae515c10580cb8d2400c74bb150f4f277d60
J ª/\/\ ∑ $ <james@galvanize.com>
2015-12-27 23:45:06 -0500

    Update objects.js

--- js/unit-8/objects.js
+++ js/unit-8/objects.js
@@ -43,13 +43,10 @@ var james = {
     }
 };
 
-// james' first job
 james.sayJob();
 
-// change james' job to "super programmer" here
 james.job = "super programmer";
 
-// james' second job
 james.sayJob();
 
 var james = {
@@ -57,9 +54,128 @@ var james = {
     married: false
 };
 
-// set to the first property name of "james"
 var aProperty = 'job';
 
-// print the value of the first property of "james" 
-// using the variable "aProperty"
 console.log(james[aProperty]);
+
+var anObj = { job: "I'm an object!" };
+var aNumber = 42;
+var aString = "I'm a string!";
+
+console.log( typeof anObj  ); 
+console.log( typeof aNumber ); 
+console.log( typeof aString );
+
+var myObj = { name: "James" };
+
+console.log( myObj.hasOwnProperty('name') ); 
+console.log( myObj.hasOwnProperty('nickname') ); 
+
+var suitcase = {
+    shirt: "Hawaiian"
+};
+
+if (suitcase.hasOwnProperty('shorts')) {
+        console.log(suitcase.shorts);
+}
+    else {
+        suitcase.shorts = "cargo jorts";
+        console.log(suitcase.shorts)
+    }
+    
+var nyc = {
+    fullName: "New York City",
+    mayor: "Bill de Blasio",
+    population: 8000000,
+    boroughs: 5
+};
+
+for(var property in nyc) {
+    console.log(property);
+}
+
+var nyc = {
+    fullName: "New York City",
+    mayor: "Bill de Blasio",
+    population: 8000000,
+    boroughs: 5
+};
+
+var j = ["fullName", "mayor", "population", "boroughs"];
+for(var j in nyc) {
+    console.log(nyc[x]);
+}
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 35);
+
+function Circle(radius) {
+    this.radius = radius;
+    }
+    
+function Dog (breed) {
+  this.breed = breed;
+}
+
+var buddy = new Dog("Golden Retriever");
+buddy.bark = function() {
+  console.log("Woof");
+};
+buddy.bark();
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark = function() {
+    console.log("Ruff");
+};
+snoopy.bark();
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+var printPersonName = function (p) {
+  console.log(p.name);
+};
+
+var bob = new Person("Bob Smith", 30);
+printPersonName(bob);
+
+var me = new Person("James", 25);
+printPerson(james);
+
+function Dog (breed) {
+  this.breed = breed;
+};
+
+var buddy = new Dog("golden Retriever");
+Dog.prototype.bark = function() {
+  console.log("Woof");
+};
+buddy.bark();
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark();
+
+function Cat(name, breed) {
+    this.name = name;
+    this.breed = breed;
+}
+
+// let's make some cats!
+var cheshire = new Cat("Cheshire Cat", "British Shorthair");
+var gary = new Cat("Gary", "Domestic Shorthair");
+
+Cat.prototype.meow = function () {
+    console.log("Meow!");
+};
+
+cheshire.meow();
+gary.meow();
