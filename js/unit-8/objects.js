322f21d12a7647908fcb3a913815358f722b13d3
J ª/\/\ ∑ $ <james@galvanize.com>
2016-01-02 09:49:57 -0500

    Update objects.js

--- js/unit-8/objects.js
+++ js/unit-8/objects.js
@@ -282,3 +282,20 @@ function Person(first,last,age) {
 }
 var john = new Person('john','smith',22);
 console.log(john.bankBalance);
+
+function Person(first,last,age) {
+   this.firstname = first;
+   this.lastname = last;
+   this.age = age;
+   var bankBalance = 7500;
+  
+   this.getBalance = function() {
+      return bankBalance;
+   };
+}
+
+var john = new Person('John','Smith',30);
+console.log(john.bankBalance);
+
+var myBalance = john.getBalance();
+console.log(myBalance);
