bd07afd5bfb69524bcfdd699cc19cd31125a1d8d
J ª/\/\ ∑ $ <james@galvanize.com>
2016-01-09 19:37:57 -0700

    Update cash-register.js

--- js/unit-8/cash-register.js
+++ js/unit-8/cash-register.js
@@ -83,5 +83,44 @@ this.discountPercent = discountPercent
 var sally = new StaffMember("Sally",5);
 var bob = new StaffMember("Bob",10);
 
-//Create a StaffMember for yourself called me
-var me = new StaffMember("JD",20);
+var me = new StaffMember("James",25);
+
+function StaffMember(name,discountPercent){
+    this.name = name;
+    this.discountPercent = discountPercent;
+}
+
+var sally = new StaffMember("Sally",5);
+var bob = new StaffMember("Bob",10);
+
+var me = new StaffMember("me",20);
+
+var cashRegister = {
+    total:0,
+    lastTransactionAmount: 0,
+    add: function(itemCost){
+        this.total += (itemCost || 0);
+        this.lastTransactionAmount = itemCost;
+    },
+    scan: function(item,quantity){
+        switch (item){
+        case "eggs": this.add(0.98 * quantity); break;
+        case "milk": this.add(1.23 * quantity); break;
+        case "magazine": this.add(4.99 * quantity); break;
+        case "chocolate": this.add(0.45 * quantity); break;
+        }
+        return true;
+    },
+    voidLastTransaction : function(){
+        this.total -= this.lastTransactionAmount;
+        this.lastTransactionAmount = 0;
+    },
+    applyStaffDiscount : function(employee) {
+        this.employee=StaffMember.prototype;
+        this.total-=this.total* (employee.discountPercent/100);     }
+    
+};
+
+cashRegister.scan('eggs',1cashRcashRegister.scan('milk',1cashRcashRegister.scan('magazine',3Register.applyStaffDiscount(me);
+// Show the total bill
+console.log('Yourl is '+cashRegister.total.toFixed(2));
