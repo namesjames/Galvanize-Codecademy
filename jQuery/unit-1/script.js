67285eb11c431744111ab5702b1ea9ccaf35db59
J ª/\/\ ∑ $ <james@galvanize.com>
2016-01-14 21:12:24 -0700

    Update script.js

--- jQuery/unit-1/script.js
+++ jQuery/unit-1/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
     $('div').fadeOut(1000);
 });
+
+$(document).ready(function() {
+    $('#green').fadeOut(1000);
+});
+
+$(document).ready(function() {
+    $('#notready').fadeOut(1000);
+});
