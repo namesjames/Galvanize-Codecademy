86b8bd7512519cb5dd0bea89656d03c11202c6ba
J ª/\/\ ∑ $ <james@galvanize.com>
2016-01-20 22:22:55 -0700

    Update script.js

--- jQuery/unit-1/script.js
+++ jQuery/unit-1/script.js
@@ -21,3 +21,12 @@ $('div').mouseenter(function() {
 $('div').fadeTo("fast", 1);
 });
 });
+
+
+$(document).ready(function() {
+$('div').mouseenter(function() {
+$('div').fadeTo("fast", 1);
+});
+$('div').mouseleave(function(){
+$('div').fadeTo("fast", 0.5);
+});
