d01269dd02e64c058ba3dc1d3e44cb44d2afb709
J ª/\/\ ∑ $ <james@galvanize.com>
2016-01-27 23:49:15 -0700

    Update script.js

--- jQuery/unit-2/script.js
+++ jQuery/unit-2/script.js
@@ -12,3 +12,32 @@ $(document).ready(function() {
 var $target = $('li:nth-child(4)');
     $target.fadeOut('fast');
 });
+
+$div = $('div');
+
+$(document).ready(function() {
+    $('div').fadeIn('slow');
+}
+);
+
+$(document).ready(function() {
+    $('button').click(function() {
+        $('.vanish').fadeOut('slow');
+    });
+});
+
+$(document).ready(function() {
+$('.red, .pink').fadeTo('slow',0);
+};
+
+$(document).ready(function() {
+    $('div').click(function() {
+        $(this).fadeOut('slow');
+    });
+});
+
+$(document).ready(function() {
+    $('.pull-me').click(function() {
+    $('.panel').slideToggle('slow');
+    });
+});
