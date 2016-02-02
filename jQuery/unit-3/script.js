970f643e06802c0044a1036a7fc7b71178af0c41
J ª/\/\ ∑ $ <james@galvanize.com>
2016-02-01 23:52:17 -0700

    Update script.js

--- jQuery/unit-3/script.js
+++ jQuery/unit-3/script.js
@@ -1,5 +1,17 @@
+$(document).ready(function(){
+    
 $h1 = $("<h1>Hello</h1>")
 
-$(document).ready(function () {
-    $("body").append('<p>"I\'m a paragraph!"</p>');
+$("body").append('<p>"I\'m a paragraph!"</p>');
+
+$('#one').after('<p>Div #1.5</p>');
+
+    var $p = $('p');
+    $('#two').after($p);    
+    
+    $('#text').click(function() {
+    $(this).addClass('highlighted');
+    });
+
 });
+
