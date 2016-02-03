3ba268817fbd26e0ea69c315d91df6582b5f0870
J ª/\/\ ∑ $ <james@galvanize.com>
2016-02-02 23:41:32 -0700

    Update script.js

--- jQuery/unit-3/script.js
+++ jQuery/unit-3/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).on('click', '.item', function() {
     
 $h1 = $("<h1>Hello</h1>")
 
@@ -13,5 +13,16 @@ $('#one').after('<p>Div #1.5</p>');
     $(this).addClass('highlighted');
     });
 
+    $("div").height("200px");
+    $("div").width("200px");
+    $("div").css("border-radius","10px");
+    
+    $('p').html("jQuery magic in action!");
+    
+    
+    $('#button').click(function() {
+        var toAdd = $('input[name=checkListItem]').val();
+        $('.list').append('<div class="item">' + toAdd + '</div>';
+    });
 });
 
