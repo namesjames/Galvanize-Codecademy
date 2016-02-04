6bdde7fae3c86a86a6c63fb5a4b428c05f1f8ce0
J ª/\/\ ∑ $ <james@galvanize.com>
2016-02-03 21:13:54 -0700

    Create script.js

---
+++ jQuery/unit-4/script.js
@@ -0,0 +1,11 @@
+$(document).ready( function() {
+   
+    $('div').click( function() {
+        $('div').fadeOut('fast');
+    }); 
+    
+    $('div').hover(function(){
+    $('div').addClass('red');
+    });
+
+});
