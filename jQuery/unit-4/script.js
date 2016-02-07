2f14d738ab5d196b9ba0a5a22751e1789d71bc60
J ª/\/\ ∑ $ <james@galvanize.com>
2016-02-06 23:36:44 -0700

    Update script.js

--- jQuery/unit-4/script.js
+++ jQuery/unit-4/script.js
@@ -8,4 +8,17 @@ $(document).ready( function() {
     $('div').addClass('red');
     });
 
+   $('div').hover(
+    function(){
+    $(this).addClass('active');
+    },
+    function(){
+    $(this).removeClass('active');    
+    }
+  );
+
+$('input').focus(function() {
+$(this).css('outline-color','#FF0000');
+})
+
 });
