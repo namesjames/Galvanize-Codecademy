64d5ab7178779373599e000b1030f2cc09d4dd3a
J ª/\/\ ∑ $ <james@galvanize.com>
2016-02-08 19:06:08 -0700

    Update script.js

--- jQuery/unit-4/script.js
+++ jQuery/unit-4/script.js
@@ -27,3 +27,22 @@ $(document).ready( function() {
    });
    
 });
+
+$(document).ready(function() {
+    $(document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
+			case 37:
+				$('img').animate({left: "-=10px"}, 'fast');
+				break;
+			case 38:
+				$('img').animate({top: "-=10px"}, 'fast');
+				break;
+			case 39:
+				$('img').animate({left: "+=10px"}, 'fast');
+				break;
+			case 40:
+				$('img').animate({top: "+=10px"}, 'fast');
+				break;
+		}
+	});
+});
