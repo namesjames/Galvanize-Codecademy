a62848409c8ead5f7bea984c818705985373a225
J ª/\/\ ∑ $ <james@galvanize.com>
2016-05-25 23:47:56 -0600

    Create practice.rb

---
+++ ruby/practice.rb
@@ -0,0 +1,15 @@
+print "Integer please: "
+user_num = Integer(gets.chomp)
+
+if user_num < 0
+  puts "You picked a negative integer!"
+elsif user_num > 0
+  puts "You picked a positive integer!"
+else
+  puts "You picked zero!"
+end
+
+if 1 < 2
+  print "I'm getting printed because one is less than two!"
+end
+
