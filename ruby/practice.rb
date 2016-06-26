56ba6e4740aa823ee373bbf244fc24c7ae823085
J ª/\/\ ∑ $ <james@galvanize.com>
2016-06-25 19:25:35 -0600

    Update practice.rb

--- ruby/practice.rb
+++ ruby/practice.rb
@@ -13,3 +13,22 @@ if 1 < 2
   print "I'm getting printed because one is less than two!"
 end
 
+hungry = false
+
+unless hungry
+  puts "I'm writing Ruby programs!"
+else
+  puts "Time to eat!"
+end
+
+is_true = 2 != 3
+
+is_false = 2 == 3
+
+test_1 = 17 >= 16
+
+test_2 = 21 <= 30
+
+test_3 = 9 >= 9
+
+test_4 = -11 < 4
