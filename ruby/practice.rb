80d63427a15afaf20bf059ed29ed359a0b64e420
J ª/\/\ ∑ $ <james@galvanize.com>
2016-06-26 23:39:31 -0600

    Update practice.rb

--- ruby/practice.rb
+++ ruby/practice.rb
@@ -32,3 +32,45 @@ test_2 = 21 <= 30
 test_3 = 9 >= 9
 
 test_4 = -11 < 4
+
+# test_1 = 77 != 77
+test_1 = false
+
+# test_2 = -4 <= -4
+test_2 = true
+
+# test_3 = -44 < -33
+test_3 = true
+
+# test_4 = 100 == 1000
+test_4 = false
+
+# boolean_1 = 77 < 78 && 77 < 77
+boolean_1 = false
+
+# boolean_2 = true && 100 >= 100
+boolean_2 = true
+
+# boolean_3 = 2**3 == 8 && 3**2 == 9
+boolean_3 = true
+
+# boolean_1 = 2**3 != 3**2 || true
+boolean_1 = true
+
+# boolean_2 = false || -10 > -9
+boolean_2 = false
+
+# boolean_3 = false || false
+boolean_3 = false
+
+# boolean_1 = !true
+boolean_1 = false
+
+# boolean_2 = !true && !true
+boolean_2 = false
+
+# boolean_3 = !(700 / 10 == 70)
+boolean_3 = false
+
+
+
